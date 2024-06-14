e23d6640752d17fa7099fb6b942d05c0ada2c6f4
RMSebastian <105923386+RMSebastian@users.noreply.github.com>
2024-06-14 15:39:07 -0300

    Task14_Socket(WiP)

--- node/express-prisma-typescript/src/server.ts
+++ node/express-prisma-typescript/src/server.ts
@@ -2,11 +2,14 @@ import express from 'express'
 import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
 
 import { Constants, NodeEnv, Logger } from '@utils'
 import { router } from '@router'
 import { ErrorHandling } from '@utils/errors'
 import { SetupSwagger } from '@utils/swagger'
+import {Server} from "socket.io";
+import { createServer } from 'http'
 
 const app = express()
 
@@ -33,6 +36,43 @@ SetupSwagger(app);
 
 app.use(ErrorHandling)
 
-app.listen(Constants.PORT, () => {
+//Socket.io integrations
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.use((socket, next) => {
+  const token = socket.handshake.headers['authorization']?.split(' ')[1];
+
+  if (!token) {
+    return next(new Error('MISSING_TOKEN'));
+  }
+
+  jwt.verify(token, Constants.TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return next(new Error('INVALID_TOKEN'));
+    }
+    next();
+  });
+});
+
+io.on('connection', (socket)=>{
+  console.log("Client Connected 👨‍🦱")
+
+  //join Room (join a chat with a friend)
+
+  //Leave Room (leave a room without the friend)
+
+  socket.on('message', (data: any)=>{
+    console.log(`Message:${socket.id} had said ${data}`);
+    socket.emit('message',`${socket.id}: ${data}`);
+    
+  });
+
+  socket.on('disconnect',()=>{
+    console.log("user disconnected");
+  })
+})
+
+httpServer.listen(Constants.PORT, () => {
   Logger.info(`Server listening on port ${Constants.PORT}`)
 })
